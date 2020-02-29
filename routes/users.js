3f993db96478cb5d076c95edd8d169b4cd8f93fa
Andrew Creigh <arcreigh@MobileDragon.local>
2020-02-29 16:29:18 -0500

    Added authentication

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-const Joi = require("@hapi/joi");
+const { User } = require("../Models/userModel");
 const express = require("express");
 const router = express.Router();
+const isAdmin = require("../isAdmin");
 
-router.get("/", (req, res) => {
-  res.send([{ user: "user1" }, { user: "user2" }, { user: "user3" }]);
+router.get("/", async (req, res) => {
+  if (await isAdmin(req)) {
+    try {
+      let users = await User.find().sort("personaName");
+      res.send(users);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    res.status(403).send("You are not an admin");
+  }
 });
 
 module.exports = router;
